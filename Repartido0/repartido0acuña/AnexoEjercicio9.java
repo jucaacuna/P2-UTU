f6b7e4a1dc8ab6aeb9896891473336631dcbf0b5
Juan Acuña <juca.acuna@gmail.com>
2024-03-06 11:09:26 -0300

    Se agrego anexo al ejercicio 9 del Repartido 0.

---
+++ Repartido0/repartido0acuña/AnexoEjercicio9.java
@@ -0,0 +1,10 @@
+package repartido0acuña;
+
+public class AnexoEjercicio9 {
+
+  /*
+   * Fragmento de El ingenioso hidalgo don Quijote de la Mancha.
+   */
+  String q = "No:h4bí4:bi3n:4c4b4do:d3:d3cir:3st4s:r4zon3s:S4ncho,:cu4ndo:ll3g4ron:4:sus:oídos:r3linchos:d3:Rocin4nt3;:los:cu4l3s:r3linchos:tomó:don:Quijot3:por:f3licísimo:4gü3ro,:y:d3t3rminó:d3:h4c3r:d3:4llí:4:tr3s:o:cu4tro:dí4s:otr4:s4lid4;:y:d3cl4r4ndo:su:int3nto:4l:B4chill3r,:l3:pidió:cons3jo:por:qué:p4rt3:com3nz4rí4:su:jorn4d4;:3l:cu4l:l3:r3spondió:qu3:3r4:su:p4r3c3r:qu3:fu3s3:4l:r3ino:d3:4r4gón:y:4:l4:ciud4d:d3:Z4r4goz4,:4dond3:d3:4llí:4:pocos:dí4s:s3:h4bí4n:d3:h4c3r:un4s:sol3mnísim4s:just4s:por:l4:fi3st4:d3:S4n:Jorg3,:3n:l4s:cu4l3s:podrí4:g4n4r:f4m4:sobr3:todos:los:c4b4ll3ros:4r4gon3s3s,:qu3:s3rí4:g4n4rl4:sobr3:todos:los:d3l:mundo.:4l4ból3:s3r:honr4dísim4:y:v4l3ntísim4:su:d3t3rmin4ción,:y:4dvirtiól3:qu3:4nduvi3s3:más:4t3nt4do:3n:4com3t3r:los:p3ligros,:4:c4us4:qu3:su:vid4:no:3r4:suy4,:sino:d3:todos:4qu3llos:qu3:l3:h4bí4n:d3:m3n3st3r:p4r4:qu3:los:4mp4r4s3:y:socorri3s3:3n:sus:d3sv3ntur4s.";
+}
+
